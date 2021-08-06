67f17e04e1b0f10d354177d4f99d145153116f25
PoMo <kaweepong.m@ku.th>
2021-08-06 18:46:22 +0700

    add page

---
+++ src/router/index.js
@@ -0,0 +1,65 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../view/Home.vue'
+import About from '../view/About.vue'
+import Login from '../view/Login.vue'
+import Register from '../view/Register.vue'
+import Landing from '../view/Landing.vue'
+import Admin from '../view/Admin.vue'
+import Reward from '../view/Reward.vue'
+import History from '../view/History.vue'
+
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/home',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register
+  },
+  {
+    path: '/',
+    name: 'Landing',
+    component: Landing
+  },
+  {
+    path: '/admin',
+    name: 'Admin',
+    component: Admin
+  },
+  {
+    path: '/reward',
+    name: 'Reward',
+    component: Reward
+  },
+  {
+    path: '/history',
+    name: 'History',
+    component: History
+  }
+  
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ src/view/Reward.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container my-3">
+    <h1 class="display-4">Reward</h1>
+    <table class="table my-3">
+      <thead>
+        <tr>
+          <th scope="col">Name</th>
+          <th scope="col">Price</th>
+          <th scope="col">Desc</th>
+          <th scope="col">#</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="reward in data" :key="reward.id">
+          <td>{{reward.name}}</td>
+          <td>{{reward.price}}</td>
+          <td>{{reward.desc}}</td>
+          <td>
+              <button class = "btn btn-success">
+                  แลก
+              </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  methods:{
+
+  },
+  created(){
+    axios.get("http://localhost:1337/water-rewards").then((res) => {
+      this.data = res.data
+      console.log(this.data);
+    })
+  },
+  data(){
+    return {
+      data:[]
+    };
+  }
+
+}
+</script>
+
+<style>
+</style>
